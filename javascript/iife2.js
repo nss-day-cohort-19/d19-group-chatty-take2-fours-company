20dae7266f7bac885ae7993bfedcd9b95412b379
preetikrp <preetikrp@gmail.com>
2017-05-02 13:21:01 -0500

    completed iife2

--- javascript/iife2.js
+++ javascript/iife2.js
@@ -1,1 +1,16 @@
 console.log("iife2 js");
+var Chatty = (function(originalChatty){
+var postLog = [];
+
+originalChatty.crDomEl = function(elementID, post){
+	var outputField = document.getElementById('MessageForum');
+	postLog.push({
+		post:post, elementID:elementID
+	})
+	    var string = `<div id="${elementID}"><p>${post}</p><button class='btnDel'>Delete</button></div>`;
+	    outputField.innerHTML += string;
+}
+
+return originalChatty;
+})(Chatty);
+
