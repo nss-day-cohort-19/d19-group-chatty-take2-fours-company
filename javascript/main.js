b7a90a628713b62029eafddb7e54d3470e4c3c04
preetikrp <preetikrp@gmail.com>
2017-05-02 13:37:55 -0500

    add new messages

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ console.log("main.js");
 
 //Initialized the variables//
 
-var messageinput = document.getElementById("TextBox");
+var messageinput = document.getElementById("userInput");
 var clearbutton = document.getElementById("Reset");
 var changeColor = document.getElementById("changeColor");
 var changeSize = document.getElementById("changeSize");
@@ -19,42 +19,19 @@ clearbutton.addEventListener("click", function(event){
 
 //added eventlistener for carriage return //
 
+var j=10; 
+
 messageinput.addEventListener("keypress", function(event){
-	//console.log(event.keyCode);
 
 
 	if (event.keyCode ===13){
-
-		MessageForum.innerHTML += messageinput.value + "<button class='deletemessage'>" + 'Delete' + "</button>" + "<br><br>";
-		messageinput.value = "";
-	//deletemessage.addEventListener("click", function(event){
-		//console.log(event. deletemessage);
-	//});
+var elementID = "post-"+j
+	Chatty.crDomEl(elementID, messageinput.value);
+j++
 
 	}
 });
 
-// Function to read content of JSON file (https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript)//
-function loadJSON(callback) {
-
-    var jsonObj = new XMLHttpRequest();
-        jsonObj.overrideMimeType("application/json");
-    jsonObj.open('GET', 'posts.JSON', true); // Replace 'my_data' with the path to your file
-    jsonObj.addEventListener = function () {
-          if (jsonObj.readyState == 4 && jsonObj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(jsonObj.responseText);
-          }
-    };
-    jsonObj.send(null);
- }
 
 
-function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    MessageForum.innerHTML = "test" +JSON.parse(response);
- });
-
-}
-init();
+Chatty.loadPosts(Chatty.crDomEl);
